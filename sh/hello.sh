5a15837e8d81c7566587757cef34f983719dbdcd
Mike Hepburn <eformat@gmail.com>
2019-11-07 11:44:24 +1000

    hello ocp4

--- sh/hello.sh
+++ sh/hello.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 name=${1:-World};
 ip=`ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1`
-while true; do echo -e "HTTP/1.1 200 OK\n\nHello ${name} ! Welcome to OpenShift 3 from ${HOSTNAME}:${ip}" | nc -ll -p 8080; done
+while true; do echo -e "HTTP/1.1 200 OK\n\nHello ${name} ! Welcome to OpenShift 4 from ${HOSTNAME}:${ip}" | nc -ll -p 8080; done
